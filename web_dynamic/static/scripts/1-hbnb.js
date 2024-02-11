05761114ca35c3ad0a1535b8110ac9344ede8834
AzorAha1 <wizfaiz@icloud.com>
2024-02-11 04:27:30 +0100

    update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,4 @@
-document.ready(function () {
+$(document).ready(function () {
   const amenities = {};
   $('li input[type=checkbox]').change(function () {
     if (this.checked) {
@@ -6,6 +6,6 @@ document.ready(function () {
     } else {
       delete amenities[this.dataset.name];
     }
-    $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
+    $('.amenities h4').text(Object.values(amenities).sort().join(', '));
   });
 });
